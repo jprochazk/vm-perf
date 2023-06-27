0f4a4b3a41e6485a86f2f3edfd67a971daaa25ac
jprochazk <1665677+jprochazk@users.noreply.github.com>
2023-06-27 19:57:58 +0200

    update bgs again

--- report.py
+++ report.py
@@ -2,7 +2,6 @@
 
 from pathlib import Path
 from subprocess import check_output
-from shutil import copyfile
 
 template = """<!DOCTYPE html>
 <html>
@@ -21,9 +20,12 @@ base = Path("target/criterion")
 for b in ["simple_loop", "nested_loop", "unpredictable"]:
     svg = base / b / "report" / "violin.svg"
     svg_out = Path("report")
-    filename = f"{b}.svg"
+    filename = f"{b}_violin.svg"
     svg_out.mkdir(parents=True, exist_ok=True)
-    copyfile(svg, svg_out / filename)
+    with open(svg, "r") as f:
+        content = f.read().replace('fill="none"/>', 'fill="white"/>', 1)
+    with open(svg_out / filename, "w") as f:
+        f.write(content)
     graphs += f'<img src="{filename}" />\n'
 
 index = template.replace("{{graphs}}", graphs)
